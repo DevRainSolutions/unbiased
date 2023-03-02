2f3acd3bbf47fcadb68d5def89315d17e2e52366
EugeneLazakovich EugeneLazakovich <y.lazakovych@devrain.com>
2023-03-02 20:18:33 +0200

    added 2 samples

---
+++ Demo.OpenAI/Pages/Sample2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo.OpenAI.Pages
+{
+    public class Sample2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
